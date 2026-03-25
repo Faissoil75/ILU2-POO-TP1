824ecfa28952b5f96ef5e60e83b1cbcbd80e694d
Faissoilm75 <faimhd1@icloud.com>
2026-03-25 19:09:59 +0100

    fFn TP1

--- src/villagegaulois/Village.java
+++ src/villagegaulois/Village.java
@@ -133,7 +133,7 @@ public class Village {
 	    return chaine.toString();
 	}
 	
-	private class Marche{
+	private static class Marche{
 		private Etal[] tabEtal;
 		
 		private Marche(int nbEtal) {
